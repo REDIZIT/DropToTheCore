1bc81e4c37e61c815152cc0a70a7d4d9507f57a2
REDIZIT <38188793+REDIZIT@users.noreply.github.com>
2020-10-21 19:39:01 +0300

    Reinstall settings log fix

--- Assets/Scripts/Secrets/SecretsManager.cs
+++ Assets/Scripts/Secrets/SecretsManager.cs
@@ -41,7 +41,7 @@ namespace InGame.Secrets
                 }
                 catch
                 {
-                    Debug.LogError("А кто тут такой мамкин хацкер? :D");
+                    //Debug.LogError("А кто тут такой мамкин хацкер? :D");
                     File.Delete(filepath);
                     _secrets = new SecretsModel();
                 }
