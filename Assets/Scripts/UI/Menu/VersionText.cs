23505d9932e49204af74d19eebb95e139e7f3899
REDIZIT <38188793+REDIZIT@users.noreply.github.com>
2021-11-24 23:48:21 +0300

    Git versioning

---
+++ Assets/Scripts/UI/Menu/VersionText.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace InGame.UI
+{
+	[RequireComponent(typeof(Text))]
+	public class VersionText : MonoBehaviour
+	{
+        private void Start()
+        {
+            GetComponent<Text>().text = Application.version;
+        }
+    }
+}
